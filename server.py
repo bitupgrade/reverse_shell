04701ebbcc55acee4c355af4bf4319156bd9a231
Arnason <stefan.arnason@dxc.com>
2020-04-04 21:42:47 +0100

    added multi threading

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 import socket
 import sys
+import threading
+import time
+from queue import Queue
+
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_addresses = []
 
 
 # Create socket
@@ -14,9 +23,8 @@ def socket_create():
     except socket.error as msg:
         print("Socket creation error: " + str(msg))
 
+
 # Bind socket to port and wait for connection from client
-
-
 def socket_bind():
     try:
         global host
@@ -29,15 +37,16 @@ def socket_bind():
         print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
         socket_bind()
 
+
+'''
 # Establish connection with client
-
-
 def socket_accept():
     conn, address = s.accept()
     print("Connection has been established | " + "IP " +
           address[0] + " | Port " + str(address[1]))
     send_commands(conn)
     conn.close()
+
 
 # Send commands
 
@@ -54,12 +63,31 @@ def send_commands(conn):
             # 1024 is the buffer size and has utf-8 encoding
             client_response = str(conn.recv(1024), "utf-8")
             print(client_response, end="")
+'''
+
+# Accept connections from multiple clients and save to list
+
+
+def accept_connections():
+    for c in all_connections:
+        c.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while True:
+        try:
+            conn, address = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(address)
+            print("\nConncetion has been established: " + address[0])
+        except:
+            print("Error accepting connections")
 
 
 def main():
     socket_create()
     socket_bind()
-    socket_accept()
+    # socket_accept()
 
 
 main()
